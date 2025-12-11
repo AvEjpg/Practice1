6faeb5eb91faf1c1bcb096cbe8a80737e49eb77b
.jpg <153241526+AvEjpg@users.noreply.github.com>
2025-12-11 17:19:56 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+# app/main.py (добавь импорты и эндпоинты)
 from fastapi import FastAPI, HTTPException
 from app.schemas.product import ProductItem, ProductCreate
+from app.repositories.products_repo import insert_product, update_product, delete_product
 from app.services.products_service import list_products_with_time
-from app.repositories.products_repo import insert_product, update_product
+
+
+from app.schemas.product_type import ProductTypeItem
+from app.schemas.material_type import MaterialTypeItem
+from app.schemas.workshop import WorkshopItem, ProductWorkshopItem
+from app.schemas.calc import RawCalcResponse
+
+from app.services.product_types_service import list_product_types
+from app.services.material_types_service import list_material_types
+from app.services.workshops_service import list_workshops_all, list_workshops_for_product
+from app.services.calc_service import calculate_raw_material
 
 app = FastAPI(title="Продукция мебельной компании")
 
@@ -22,5 +34,34 @@ def edit_product(product_id: int, product: ProductCreate):
 @app.delete("/products/{product_id}")
 def remove_product(product_id: int):
     delete_product(product_id)
-
     return {"message": "Product deleted successfully"}
+
+# Справочники
+@app.get("/product_types", response_model=list[ProductTypeItem])
+def get_product_types():
+    return list_product_types()
+
+@app.get("/material_types", response_model=list[MaterialTypeItem])
+def get_material_types():
+    return list_material_types()
+
+# Цеха
+@app.get("/workshops", response_model=list[WorkshopItem])
+def get_workshops():
+    return list_workshops_all()
+
+@app.get("/products/{product_id}/workshops", response_model=list[ProductWorkshopItem])
+def get_product_workshops(product_id: int):
+    return list_workshops_for_product(product_id)
+
+# Расчет сырья
+@app.get("/calculate_raw_material", response_model=RawCalcResponse)
+def calculate_raw_material_endpoint(
+    product_type_id: int,
+    material_type_id: int,
+    quantity: int,
+    param1: float,
+    param2: float,
+):
+    result = calculate_raw_material(product_type_id, material_type_id, quantity, param1, param2)
+    return {"result": result}
